e3e5fe5675756e4e694ed9525487a8638041451d
Mikko <mikko.kokkonen@outlook.com>
2025-02-05 20:38:45 +0200

    DNS query operation added

--- Services/Clients/TechniClient.cs
+++ Services/Clients/TechniClient.cs
@@ -1,14 +1,42 @@
-﻿namespace InternetSentry.Services.Clients
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace InternetSentry.Services.Clients
 {
     public sealed class TechniClient(ILogger<TechniService> logger, IConfiguration config)
     {
         private ILogger<TechniService> _logger = logger;
         private IConfiguration _clientConfig = config.GetSection("Techni");
+        private IConfiguration _testDestinations = config.GetSection("Destinations");
 
 
         public async Task CheckInternetStatus()
         {
+            bool didFail = false;
+            try
+            {
+                string dnsName = _testDestinations.GetSection("DNS").Value ?? "";
+                if (!string.IsNullOrEmpty(dnsName))
+                {
+                    IPHostEntry iPHost = await Dns.GetHostEntryAsync(dnsName);
+                }
+                else
+                {
+                    _logger.LogWarning($"{GetType().Name} DNS name not defined in configs ");
+                    didFail = true;
+                }
+            }
+            catch (SocketException se)
+            {
+                _logger.LogWarning($"{GetType().Name} DNS query failed", se);
+                didFail = true;
+            }
 
+
+            if (didFail) {
+                _logger.LogWarning($", proceed on restarting router");
+                await ForceRestart();
+            }
         }
 
         public async Task ForceRestart()
